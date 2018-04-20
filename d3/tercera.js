a107a3aaeb53a0e35f20713cf147e8c774fe560d
Constanza Cea <37454268+connycea22@users.noreply.github.com>
2018-04-20 18:24:34 -0300

    Segunda evaluación
    20/04/18

---
+++ d3/tercera.js
@@ -0,0 +1,9 @@
+//aquí debes trabajar con datos de un CSV
+var w = 500;
+var h = 450;
+d3.select(".dataviz-tercera")
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h )
+	.style("background","url(http://images-ahn.mdstrm.com/2017/09/29/206626_1_59ce4d3d397fd.jpg?d=800x400)")
+    .style("background-size","cover");
